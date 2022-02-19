6007cc4c905c06b282af76a477bb52b517535bf3
Marvin Salazar <gerardosalazar2231@gmail.com>
2022-02-19 16:01:00 -0600

    agregando set y get

--- Propietario.cpp
+++ Propietario.cpp
@@ -13,6 +13,23 @@ class Propietario : Persona{
 				nit=n;
 				cui=c;
 			}
+
+	// set (modificar)
+	void setNit(string n){	nit = n;}
+	void setCui(double c){ cui=c;}
+	void setNombres(string nom){	nombres = nom;}
+	void setApellidos(string ape){	apellidos = ape;}
+	void setDireccion(string dir){	direccion = dir;}
+	void setTelefono(int tel){	telefono = tel;}
+	void setFN(string f){	fn = f;}
+	// get (mostrar)
+	string getNit(){return nit;}
+	double getCui(){return cui;}
+	string getNombres(){return nombres;}
+	string getApellidos(){return apellidos;}
+	string getDireccion(){return direccion;}
+	string getFN(){return fn;}
+	int getTelefono(){return telefono;}
 				
 		void mostrar(){
 		cout<<"----------Metodo mostrar------------"<<endl;
